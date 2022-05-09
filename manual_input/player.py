0a763ecab56088ccdc2b25c41534e7738b0be14e
Abrar Yusuf <abrar.h.yusuf@gmail.com>
2022-05-10 00:40:09 +1000

    Added internal board representation

--- manual_input/player.py
+++ manual_input/player.py
@@ -1,15 +1,6 @@
-from numpy import zeros
 
-
-_PLACE_ACT_LEN = 3
-_TOKEN_MAP_OUT = { 0: None, 1: "red", 2: "blue" }
-_TOKEN_MAP_IN = {v: k for k, v in _TOKEN_MAP_OUT.items() }
-_ACT_X_POS = 1
-_ACT_Y_POS = 2
 
 class Player:
-
-    
     def __init__(self, player, n):
         """
         Called once at the beginning of a game to initialise this player.
@@ -19,18 +10,15 @@ class Player:
         play as Red, or the string "blue" if your player will play
         as Blue.
         """
-        self.n = n
-        self._data = zeros((n,n), dtype=int)
-        print(self._data)
+        
 
     def action(self):
         """
         Called at the beginning of your turn. Based on the current state
         of the game, select an action to play.
         """
-        x = int(input("\nSelect x: "))
-        y = int(input("\nSelect y: "))
-        
+        x = int(input("Select x (manual): "))
+        y = int(input("Select y (manual): "))
         return ("PLACE", x, y)
     
     def turn(self, player, action):
@@ -45,22 +33,4 @@ class Player:
         above. However, the referee has validated it at this point.
         """
         
-        if len(action) == _PLACE_ACT_LEN:
-            x = self.axial_x(action[_ACT_X_POS])  
-            y = action[_ACT_Y_POS]
-            self._data[x][y] = _TOKEN_MAP_IN[player]
-        print(self._data)
-
-
-    def axial_x(self, x):
-        """
-        Flips the value of the x-coordinate along the middle of the board to
-        get an axial x-coordinate.
-        """
-        mid = int(self.n / 2)
-        x = (mid - x) + mid
-        return x
-
-        
-        # put your code here
 
